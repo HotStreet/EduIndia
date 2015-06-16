0696543b0059f9cdce3fc33ce6002dd4bc170174
RUCHI MUTNEJA <ruchimut@thoughtworks.com>
2015-06-16 15:33:19 +0530

    getting all student data, hittin api

--- app/src/main/java/hotstreet/com/eduindia/HomeActivity.java
+++ app/src/main/java/hotstreet/com/eduindia/HomeActivity.java
@@ -1,17 +1,43 @@
 package hotstreet.com.eduindia;
 
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import org.json.JSONObject;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 
 public class HomeActivity extends ActionBarActivity {
+
+    public static final String STUDENTS_INDEX_URL= "http://eduindia.herokuapp.com/students.json";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_activity);
+        ListView listView = (ListView)findViewById(R.id.list);
+        AsyncTask<String, Void, String> students = getAllStudents();
+
+//        for(int i =0;i<students.length();i++){
+
+//        }
+//        ArrayAdapter<String> studentsAdapters = new ArrayAdapter<String>(this, R.id.kuj_vi, students);
+//        listView.setAdapter(studentsAdapters);
+        try {
+            String values = students.get();
+//            JSONObject jsonObject = new JSONObject(values.);
+//            for(int i=0;i<values.length;i++)
+            System.out.println(values+">>>>>>>");
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -34,5 +60,10 @@ public class HomeActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public android.os.AsyncTask<String, Void, String> getAllStudents(){
+        JsonParser jsonParser = new JsonParser();
+        return jsonParser.execute(STUDENTS_INDEX_URL);
     }
 }
---
+++ app/src/main/java/hotstreet/com/eduindia/JsonParser.java
@@ -0,0 +1,64 @@
+package hotstreet.com.eduindia;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
+public class JsonParser extends AsyncTask<String, Void, String> {
+
+    static InputStream is = null;
+    static JSONObject json = null;
+    static String outPut = "";
+
+    @Override
+    public String doInBackground(String... url) {
+
+        // Making the HTTP request
+        try {
+
+            DefaultHttpClient httpClient = new DefaultHttpClient();
+            HttpGet httpGet = new HttpGet(url[0]);
+
+            HttpResponse httpResponse = httpClient.execute(httpGet);
+            HttpEntity httpEntity = httpResponse.getEntity();
+            is = httpEntity.getContent();
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    is, "iso-8859-1"), 8);
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = in.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            is.close();
+            outPut = sb.toString();
+            Log.e("JSON", outPut);
+        } catch (Exception e) {
+            Log.e("Buffer Error", "Error converting result " + e.toString());
+        }
+
+        return outPut;
+    }
+
+}
